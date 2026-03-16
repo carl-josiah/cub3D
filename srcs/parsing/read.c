406aeee41b7dc32ccca204d35e046aa3f1059283
carl-josiah <carl_josiah@yahoo.com>
2026-03-16 12:19:41 +0000

    fix(read.c): no leaks are possible

--- srcs/parsing/read.c
+++ srcs/parsing/read.c
@@ -6,7 +6,7 @@
 /*   By: ccastro <ccastro@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 19:19:22 by ccastro           #+#    #+#             */
-/*   Updated: 2026/03/11 17:53:53 by ccastro          ###   ########.fr       */
+/*   Updated: 2026/03/16 16:19:11 by ccastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ static char	**get_lines(int fd, size_t line_count)
 		if (!line)
 		{
 			free_double_ptr(lines);
+			while ((line = get_next_line(fd)))
+				free(line);
 			return (NULL);
 		}
 		lines[i] = line;
@@ -57,6 +59,7 @@ char	**read_cub_file(const char *cub_file, size_t *lc)
 {
 	int		fd;
 	char	**lines;
+	char	*tmp;
 
 	fd = open(cub_file, O_RDONLY);
 	if (fd == -1)
@@ -67,6 +70,8 @@ char	**read_cub_file(const char *cub_file, size_t *lc)
 	if (fd == -1)
 		return (NULL);
 	lines = get_lines(fd, *lc);
+	while ((tmp = get_next_line(fd)))
+		free(tmp);
 	close(fd);
 	if (!lines)
 		return (NULL);
